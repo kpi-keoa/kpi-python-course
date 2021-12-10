bc502bfc313246061464c1626832b56d009d5a2a
NazarDiadiun <55437433+NazarDiadiun@users.noreply.github.com>
2021-12-10 17:41:51 +0200

    Add files via upload

--- dk92_diadiun/lab1_func/Calculator.py
+++ dk92_diadiun/lab1_func/Calculator.py
@@ -1,10 +1,22 @@
 # /usr/local/bin/python3 /Users/NazarDiadiun/Desktop/Calculator.py "24 + 46"
 
+import os
 import sys
 import time
 import re
 
+os.system('clear')
+
 def Calculator(ToCalculate, NumbersList, Mode):
+    """ Функция для просчёта указанной строки.
+
+    Принимает на вход три аргумента:
+        - Строку, которая должна быть просчитана
+        - Список чисел, из которых состоит строка
+        - Цикл, который будет использован. 1 - for, 2 - while
+    
+    Возвращает результат счёта.
+    """
     ListIndex = 1
     CalculationResult = int(NumbersList[0])
 
@@ -43,29 +55,35 @@ def Calculator(ToCalculate, NumbersList, Mode):
 
     if Mode == 1:
         print(" Время выполнения for %0.5f миллисекунд" % ((time.time() - start_time) * 1000))
-    if Mode == 2:
+    elif Mode == 2:
         print(" Время выполнения while %0.5f миллисекунд" % ((time.time() - start_time) * 1000))
 
     return CalculationResult
 
-while True:
 
+def data_input():
+    """Функция принимает данные для дальнейшей обработки в програме
+        - Можно получить аргументы из командной строки
+        - Можно задать непосредственно во время выполнения программы
+    """
     # Если переданы аргументы через командную строку
     if len(sys.argv) > 1:
     # В строку, для дальнейших операций, заносим первый аргумент
-        InputString = sys.argv[1]
+        return sys.argv[1]
     # Если аргументов не передали
     else:
     # Запрашиваем у пользователя
-        InputString = input("Что там посчитать?:");
+        return input("Что там посчитать?:")
 
+
+InputString = data_input()
+
+os.system('clear')
     
     # Передача аргументов как позиционных
-    print("Результат:", Calculator(InputString, re.split('\+|\-|\*|\/', InputString), 1))
-    print("Результат:", Calculator(InputString, re.split('\+|\-|\*|\/', InputString), 2))
+print("Результат:", Calculator(InputString, re.split('\+|\-|\*|\/', InputString), 1))
+print("Результат:", Calculator(InputString, re.split('\+|\-|\*|\/', InputString), 2))
 
     # Как ключевых
     # print("Результат:", Calculator(NumbersList=re.split('\+|\-|\*|\/', InputString), ToCalculate=InputString, Mode=1))
 
-
-    break
