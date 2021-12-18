ab7c520b6d84cd77b044d26b355eeddfd0775f7f
NazarDiadiun <55437433+NazarDiadiun@users.noreply.github.com>
2021-12-18 22:02:46 +0200

    Updated Lab1 file

--- dk92_diadiun/lab1_func/Calculator.py
+++ dk92_diadiun/lab1_func/Calculator.py
@@ -1,11 +1,20 @@
 # /usr/local/bin/python3 /Users/NazarDiadiun/Desktop/Calculator.py "24 + 46"
 
+import operator as op
 import os
 import sys
 import time
 import re
 
 os.system('clear')
+
+operator_map = {
+    '+': op.add,
+    '-': op.sub,
+    '*': op.mul,
+    '/': op.truediv
+}
+
 
 def Calculator(ToCalculate, NumbersList, Mode):
     """ Функция для просчёта указанной строки.
@@ -14,8 +23,6 @@ def Calculator(ToCalculate, NumbersList, Mode):
         - Строку, которая должна быть просчитана
         - Список чисел, из которых состоит строка
         - Цикл, который будет использован. 1 - for, 2 - while
-    
-    Возвращает результат счёта.
     """
     ListIndex = 1
     CalculationResult = int(NumbersList[0])
@@ -23,40 +30,27 @@ def Calculator(ToCalculate, NumbersList, Mode):
     start_time = time.time()
     if Mode == 1:
         for Counter in range(len(ToCalculate)):
-            if ToCalculate.find('+', Counter, Counter + 1) != -1:
-                CalculationResult += int(NumbersList[ListIndex])
-                ListIndex += 1
-            if ToCalculate.find('-', Counter, Counter + 1) != -1:
-                CalculationResult -= int(NumbersList[ListIndex])
-                ListIndex += 1
-            if ToCalculate.find('*', Counter, Counter + 1) != -1:
-                CalculationResult *= int(NumbersList[ListIndex])
-                ListIndex += 1
-            if ToCalculate.find('/', Counter, Counter + 1) != -1:
-                CalculationResult /= int(NumbersList[ListIndex])
-                ListIndex += 1
+            for op, func in operator_map.items():
+                if ToCalculate.find(op, Counter, Counter + 1) != -1:
+                    CalculationResult = func(
+                        CalculationResult, int(NumbersList[ListIndex]))
+                    ListIndex += 1
     if Mode == 2:
         Counter = 0
         while Counter < len(ToCalculate):
-            if ToCalculate.find('+', Counter, Counter + 1) != -1:
-                CalculationResult += int(NumbersList[ListIndex])
-                ListIndex += 1
-            if ToCalculate.find('-', Counter, Counter + 1) != -1:
-                CalculationResult -= int(NumbersList[ListIndex])
-                ListIndex += 1
-            if ToCalculate.find('*', Counter, Counter + 1) != -1:
-                CalculationResult *= int(NumbersList[ListIndex])
-                ListIndex += 1
-            if ToCalculate.find('/', Counter, Counter + 1) != -1:
-                CalculationResult /= int(NumbersList[ListIndex])
-                ListIndex += 1
+            for op, func in operator_map.items():
+                if ToCalculate.find(op, Counter, Counter + 1) != -1:
+                    CalculationResult = func(
+                        CalculationResult, int(NumbersList[ListIndex]))
+                    ListIndex += 1
             Counter += 1
 
-
     if Mode == 1:
-        print(" Время выполнения for %0.5f миллисекунд" % ((time.time() - start_time) * 1000))
+        print(" Время выполнения for %0.5f миллисекунд" % (
+            (time.time() - start_time) * 1000))
     elif Mode == 2:
-        print(" Время выполнения while %0.5f миллисекунд" % ((time.time() - start_time) * 1000))
+        print(" Время выполнения while %0.5f миллисекунд" % (
+            (time.time() - start_time) * 1000))
 
     return CalculationResult
 
@@ -66,24 +60,20 @@ def data_input():
         - Можно получить аргументы из командной строки
         - Можно задать непосредственно во время выполнения программы
     """
-    # Если переданы аргументы через командную строку
     if len(sys.argv) > 1:
-    # В строку, для дальнейших операций, заносим первый аргумент
         return sys.argv[1]
-    # Если аргументов не передали
     else:
-    # Запрашиваем у пользователя
         return input("Что там посчитать?:")
 
 
 InputString = data_input()
 
 os.system('clear')
-    
-    # Передача аргументов как позиционных
-print("Результат:", Calculator(InputString, re.split('\+|\-|\*|\/', InputString), 1))
-print("Результат:", Calculator(InputString, re.split('\+|\-|\*|\/', InputString), 2))
+print("Результат:", Calculator(InputString, re.split(
+    '\+|\-|\*|\/', InputString), 1))  # Передача аргументов как позиционных
+print("Результат:", Calculator(InputString, re.split(
+    '\+|\-|\*|\/', InputString), 2))  # Передача аргументов как позиционных
 
-    # Как ключевых
-    # print("Результат:", Calculator(NumbersList=re.split('\+|\-|\*|\/', InputString), ToCalculate=InputString, Mode=1))
-
+# Как ключевых
+# print("Результат:", Calculator(NumbersList=re.split(
+#   '\+|\-|\*|\/', InputString), ToCalculate=InputString, Mode=1))
