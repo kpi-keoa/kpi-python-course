608b4fc66fac8c6d4f5e7cc569c59d6cfe1d70f9
TerminatorNemo <arceni.2002@gmail.com>
2021-12-23 13:16:45 +0200

    LR_2

--- dk92_zagreba/lab2_oop/LR_2.py
+++ dk92_zagreba/lab2_oop/LR_2.py
@@ -1,3 +1,5 @@
+
+from functools import total_ordering
 class Component:
     def temperature(self, t):
         self.temp = t
@@ -6,7 +8,7 @@ class Component:
 @total_ordering
 class Resistor(Component):
     def __init__(self, ohm, t):
-        self.om = om
+        self.ohm = ohm
         self.temperature(t)
 
     def __eq__(self, other):
@@ -16,7 +18,7 @@ class Resistor(Component):
         return self.ohm < other.ohm
 
     def __repr__(self):  # меняет отображение в системе
-        return f"Резистор нежен для добавления сопротивления в {self.om} Om"
+        return f"Резистор нежен для добавления сопротивления в {self.ohm} Om"
 
 
 class Сapaсitor(Component):
@@ -28,7 +30,7 @@ class Сapaсitor(Component):
         return f"Конденсатор нужен для добавления емкости в {self.far} Фарад"
 
 
-class CapacitorSMD(capasitor):
+class CapacitorSMD(Сapaсitor):
     def __init__(self, Far, t, tip):
         self.far = Far
         self.temperature(t)
@@ -39,13 +41,13 @@ if __name__ == "__main__":
 
     def _demonstrate_():
         print("\nДемонстрация роботы")
-        rezist = rezistor(500, 20)
-        print(rezist.om, "om", rezist.temp, "t", rezist)
-        cap = capasitor(50, 20)
+        rezist = Resistor(500, 20)
+        print(rezist.ohm, "ohm", rezist.temp, "t", rezist)
+        cap = Сapaсitor(50, 20)
         print(cap.far, "Farad", cap.temp, "t", cap)
-        cap_smd = capasitor_smd(10, 240, 8163)
+        cap_smd = CapacitorSMD(10, 240, 8163)
         print(cap_smd.far, "Farad", cap_smd.temp, "t", cap_smd.tipe, "tip", cap_smd)
-        rezist_2 = rezistor(500, 20)
+        rezist_2 = Resistor(500, 20)
         if rezist_2 == rezist:
             print("==")
         if rezist_2 <= rezist:
