4b147e0c86ada0b6cde2403f2594185868cbd64c
Alina Tymchenko <alina.timchenko01@gmail.com>
2021-12-27 14:29:13 +0200

    Fixed Lab3

--- dk91_atymchenko/lab3/rand_gen.py
+++ dk91_atymchenko/lab3/rand_gen.py
@@ -5,67 +5,68 @@ import random
 from functools import partial
 from math import ceil
 from pathlib import Path
-from transliterate import translit
+from transliterate import translator
 from yaml import safe_load
+
 
 LANG = 'uk'
 
-translit = partial(translit, language_code=LANG, reversed=True)
-
-def chunker(seq, size):
-    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+"""This function translates student list into English."""
+translator = lambda value: translit(value, language_code = LANG, reversed = True)
 
 
 def parse_students(filename, lang = LANG):
+    """This function replaces the student list with translated ones."""
     with open(filename) as f:
-        dct = safe_load(f)
-        dct =
-        {
-            translit(key):
-            """Function replaces all the blank lines with underscores"""
-            [(translit(v).replace(' ', '_'), v) for v in values]
-            for key, values in dct.items()
-        }
-        return dct
+        dictionary = safe_load(f)
+        dictionary = {translator(key): [(translator(v).replace(' ', '_'), v)
+                               for v in values]
+               for key, values in dictionary.items()}
+        return dictionary
 
 
 def read_questions(directory):
+    """Function sorts questions from the specific directory."""
     pth = Path(directory)
-    dct = {}
+    dictionary = {}
     for file in sorted(list(pth.glob('*.*'))):
         with open(file) as f:
             qst = f.read().strip().split('\n#')
             qst[1:] = ['#' + q for q in qst[1:]]
-            dct[file.name] = qst
-    return dct
+            dictionary[file.name] = qst
+
+    return dictionary
 
 
-def generate_questions(students, questions, *, numeach=2):
+def generate_questions(students, questions, *, numeach = 2):
+    """This function generates an individual task for each student."""
     sep = '=' * 30 + '\n'
     files = dict()
     for group, lst in students.items():
         for tr, orig in lst:
-            files[f"{group}_{tr}.rst"] = f"{sep}{orig}\n{sep}\n"
+            files[f'{group}_{tr}.rst'] = f'{sep}{orig}\n{sep}\n'
 
     for catname, category in questions.items():
-        print(f'{catname:<40}', end = '\t')
+        print(f'{catname:<40}', end='\t')
         personal = list(itertools.combinations(category, numeach))
         print(f'{len(personal)} combinations')
         assert len(personal) >= len(files)
         random.shuffle(personal)
         for f in files:
-            files[f] += '\n\n'.join(personal.pop(0))'\n'
+            files[f] += '\n\n'.join(personal.pop(0)) + '\n'
+
     return files
 
 
 def write_files(outdir, files):
+    """Function creates a file for each student with individual questions."""
     odir = Path(outdir)
     odir.mkdir(exist_ok = True)
     for filename, contents in files.items():
         file = odir.joinpath(filename)
         with open(file, 'x') as f:
             f.write(contents)
-        
+
 
 if __name__ == '__main__':
     from sys import argv
@@ -73,7 +74,7 @@ if __name__ == '__main__':
         print(f'Usage\n{argv[0]} student_list questions_dir out_dir')
         exit(-1)
     student_list, questions_dir, out_dir = argv[1:]
-    st = parse_students(student_list)
-    q = read_questions(questions_dir)
-    cont = generate_questions(st, q, numeach = 2)
+    students = parse_students(student_list)
+    questions = read_questions(questions_dir)
+    cont = generate_questions(students, questions, numeach = 2)
     write_files(out_dir, cont)
