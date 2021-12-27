6881e127cc75295283a2e288d404ccf67658507b
Alina Tymchenko <alina.timchenko01@gmail.com>
2021-12-27 16:01:55 +0200

    Finally fixed Lab3

--- dk91_atymchenko/lab1/lab1.py
+++ dk91_atymchenko/lab1/lab1.py
@@ -7,26 +7,23 @@ d = 0
 a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
-
-def discriminant (a, b, c):
-    d = b ** 2 - 4 * a * c
-    print ("Discriminant = %.1f" % d)
-
-def roots (a, b, c):
+    
+def roots (a, b, c, d):
     """This function culculates the roots of quadratic equation.
 
     We have two possible roots if discriminant > 0, but if discriminant is 0 then we have only one 
     root. If discriminant is less then 0, there are no roots. To be simplier with getting the result
     roots are declared as an array"""
 
+    d = b ** 2 - 4 * a * c
+
     if d > 0:
         x[0] = (-b + math.sqrt(d)) / (2 * a)
         x[1] = (-b - math.sqrt(d)) / (2 * a)
         print("x1 = %.1f \nx2 = %.1f" % (x[0], x[1]))
-    elif d == 0:
+    elif d < 0:
+        print("There are no roots")
+    else:
         x[0] = (-b) / (2 * a)
         print("x = %.1f" % x[0])
-    else:
-        print("There are no roots")
-discriminant (a, b, c)
-roots (a, b, c)
+roots (a, b, c, d)
