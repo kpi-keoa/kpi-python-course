4072e8bdad6776aac5f21ea8e08dfe79a4636222
NazarDiadiun <NazarDiadiun@gmail.com>
2021-12-28 08:58:11 +0200

    lab 2 oop file

--- dk92_diadiun/lab2_oop/oop.py
+++ dk92_diadiun/lab2_oop/oop.py
@@ -1,157 +1,159 @@
 import os
+import sys
 
-os.system('clear')
+if sys.platform == 'linux':
+    def clear_screen():
+        print('\033[H\033[2J', end='')
+else:
+    def clear_screen():
+        os.system('cls')
+
+clear_screen()
 
 
 class Device:
     """Device class.
     Accepting parameters:
-    - Model
-    - Year
-    - Generation
+    - model
+    - year
+    - generation
     - Pencil Connected
     - Battery charge
     """
 
-    Model = ""
-    Year = 0
-    Generation = 0
-    PencilConnected = False
-    BatteryCharge = 0
-
-    def __init__(self, Model="Unavailable", Year=0,
-                 Generation=0, Pencil=False, Battery=0):
+    def __init__(self, model="Unavailable", year=0,
+                 generation=0, Pencil=False, Battery=0):
         """Device Class Constructor"""
-        self.Model = Model
-        self.Year = Year
-        self.Generation = Generation
-        self.PencilConnected = Pencil
-        self.BatteryCharge = Battery
+        self.model = model
+        self.year = year
+        self.generation = generation
+        self.pencil_connected = Pencil
+        self.battery_charge = Battery
 
     def get_model(self):
         """Function prints device model, if available.
         Else prints unknown model
         """
-        if(self.Model != "Unavailable\n"):
-            print(self.Model)
+        if(self.model != "Unavailable\n"):
+            print(self.model)
         else:
-            print("Unknown Model\n")
+            print("Unknown model\n")
 
     def get_year(self):
         """Function prints device year, if available"""
-        if (self.Year != 0):
-            print(self.Model, "Device Year:", self.Year, "\n")
+        if (self.year != 0):
+            print(self.model, "Device year:", self.year, "\n")
 
     def get_generation(self):
         """Function prints device generation, if available"""
-        if (self.Model != "Unavailable" and self.Generation != 0):
-            print(self.Model, "Device Generation:", self.Generation, "\n")
+        if (self.model != "Unavailable" and self.generation != 0):
+            print(self.model, "Device generation:", self.generation, "\n")
 
     def get_battery(self):
         """Function prints device battery charge, in %"""
-        print(self.Model, "Battery Charge:", self.BatteryCharge, "%\n")
+        print(self.model, "Battery Charge:", self.battery_charge, "%\n")
 
-    def set_battery(self, newBatteryCharge):
+    def set_battery(self, newbattery_charge):
         """Function sets new battery charge"""
-        self.BatteryCharge = newBatteryCharge
-        print(self.Model, "new battery charge:", self.BatteryCharge, "%\n")
+        self.battery_charge = newbattery_charge
+        print(self.model, "new battery charge:", self.battery_charge, "%\n")
 
     def __str__(self):
-        DeviceLog = "Device Model: " + str(self.Model) + "\n"
+        device_log = "Device model: " + str(self.model) + "\n"
 
         if(self.__class__.__name__ != "Pencil"):
-            DeviceLog = DeviceLog + "Model Year: " + \
-                        str(self.Year) + "\n"
+            device_log = device_log + "model year: " + \
+                        str(self.year) + "\n"
         if(self.__class__.__name__ != "Laptop"):
-            DeviceLog = DeviceLog + "Device Generation: " + \
-                        str(self.Generation) + "\n"
+            device_log = device_log + "Device generation: " + \
+                        str(self.generation) + "\n"
         if(self.__class__.__name__ == "Tablet"):
-            DeviceLog = DeviceLog + "Is Pencil Connected: " + \
-                        str(self.PencilConnected) + "\n"
-        DeviceLog = DeviceLog + "Battery Charge: " + \
-                                str(self.BatteryCharge) + "%\n"
-        return DeviceLog
+            device_log = device_log + "Is Pencil Connected: " + \
+                        str(self.pencil_connected) + "\n"
+        device_log = device_log + "Battery Charge: " + \
+                    str(self.battery_charge) + "%\n"
+        return device_log
 
 
 class Laptop(Device):
     """Laptop Class.
     Accepting parameters:
-    - Model
-    - Year
+    - model
+    - year
     - Battery charge
     """
 
-    def __init__(self, Model="Unavailable", Year=0, BatteryCharge=0):
+    def __init__(self, model="Unavailable", year=0, battery_charge=0):
         """Laptop Class Constructor"""
-        self.Model = Model
-        self.Year = Year
-        self.Generation = 0
-        self.PencilConnected = False
-        self.BatteryCharge = BatteryCharge
+        self.model = model
+        self.year = year
+        self.generation = 0
+        self.pencil_connected = False
+        self.battery_charge = battery_charge
 
 
 class Tablet(Device):
     """Tablet Class.
     Accepting parameters:
-    - Model
-    - Generation
-    - Year
+    - model
+    - generation
+    - year
     - Pencil Connected
     - Battery charge
     """
 
-    def __init__(self, Model="Unavailable", Year=0, Generation=0,
-                 PencilConnected=False, BatteryCharge=0):
+    def __init__(self, model="Unavailable", year=0, generation=0,
+                 pencil_connected=False, battery_charge=0):
         """Tablet Class Constructor"""
 
-        self.Model = Model
-        self.Year = Year
-        self.Generation = Generation
-        self.PencilConnected = PencilConnected
-        self.BatteryCharge = BatteryCharge
+        self.model = model
+        self.year = year
+        self.generation = generation
+        self.pencil_connected = pencil_connected
+        self.battery_charge = battery_charge
 
     def get_pencil(self):
         """Function prints pencil connection status"""
-        print(self.Model, "Pencil Connected:", self.PencilConnected, "\n")
+        print(self.model, "Pencil Connected:", self.pencil_connected, "\n")
 
-    def set_pencil(self, newPencilConnected):
+    def set_pencil(self, newpencil_connected):
         """Function changes pencil connection status"""
-        self.PencilConnected = newPencilConnected
-        print(self.Model, "Pencil Now Connected?:", self.PencilConnected, "\n")
+        self.pencil_connected = newpencil_connected
+        print(self.model, "Pencil Connected?:", self.pencil_connected, "\n")
 
 
 class Pencil(Device):
     """Pencil Class.
     Accepting parameters:
-    - Generation
+    - generation
     - Battery Charge
     """
 
-    def __init__(self, Model="Unavailable", Generation=0, BatteryCharge=0):
+    def __init__(self, model="Unavailable", generation=0, battery_charge=0):
         """Pencil Class Constructor"""
-        self.Model = Model
-        self.Year = 0
-        self.Generation = Generation
-        self.PencilConnected = False
-        self.BatteryCharge = BatteryCharge
+        self.model = model
+        self.year = 0
+        self.generation = generation
+        self.pencil_connected = False
+        self.battery_charge = battery_charge
 
 
 if(__name__ == '__main__'):
-    MacbookPro = Laptop("Macbook Pro", 2019, 75)
-    IPadPro = Tablet("IPad Pro", 2016, 2, True, 98)
-    IPadAir = Tablet("IPad Air", 2018, 3, False, 23)
+    macbook_pro = Laptop("Macbook Pro", 2019, 75)
+    ipad_pro = Tablet("IPad Pro", 2016, 2, True, 98)
+    ipad_air = Tablet("IPad Air", 2018, 3, False, 23)
     IPencil = Pencil("IPencil", 1, 100)
 
-    MacbookPro.get_battery()
-    IPadAir.get_generation()
-    IPadPro.get_pencil()
+    macbook_pro.get_battery()
+    ipad_air.get_generation()
+    ipad_pro.get_pencil()
 
-    MacbookPro.set_battery(50)
+    macbook_pro.set_battery(50)
 
-    IPadAir.set_pencil(True)
-    IPadAir.get_pencil()
+    ipad_air.set_pencil(True)
+    ipad_air.get_pencil()
 
-    print(MacbookPro)
-    print(IPadPro)
-    print(IPadAir)
+    print(macbook_pro)
+    print(ipad_pro)
+    print(ipad_air)
     print(IPencil)
