1babf8d90a97ce9ccb01a59965eef7ec692c0123
Kateryna Tymchenko <kate.timchenko.2001@gmail.com>
2022-01-09 22:54:31 +0200

    Lab 3. Fixed version

--- dk91_ktymchenko/lab2-3/README.rst
+++ dk91_ktymchenko/lab2-3/README.rst
@@ -3,3 +3,10 @@ To demonstrate class mastery in python, a script was written containing: the par
 - The Film class contains a description of the movie consisting of one part or a description of each part separately. 
 - The Serial class describes the title, genre, number of seasons and years of the series. 
 - The Season class displays information about a random season of the series.
+
+------------------------------------lab 3----------------------------------------
+- comments have been deleted;
+- docstring was added;
+- a separate function has been created to translate names;
+- the question divider has been set as a global variable;
+- variable names were changed for better understanding
--- dk91_ktymchenko/lab2-3/rand_gen.py
+++ dk91_ktymchenko/lab2-3/rand_gen.py
@@ -10,58 +10,58 @@ from yaml import safe_load
 SEP = "=" * 30 + "\n"
 LANG = "uk"
 
-translit = partial(translit, language_code=LANG, reversed=True)
-
+def translate(value):
+    return translit(value, language_code=LANG, reversed=True)
 
 def chunker(seq, size):
     return (seq[pos : pos + size] for pos in range(0, len(seq), size))
 
 
-def parseStudents(filename, lang=LANG):
+def parse_students(filename, lang=LANG):
     with open(filename) as file:
-        studNameDict = safeLoad(file)
-        studNameDict = {
-            translit(key): [(translit(value).replace(" ", "_"), v) for v in values]
-            for key, values in studNameDict.items()
+        stud_name_dict = safe_load(file)
+        stud_name_dict = {
+            translit(key): [(translit(value).replace(" ", "_"), value) for value in values]
+            for key, values in stud_name_dict.items()
         }
-        return studNameDict
+        return stud_name_dict
 
 
-def readQuestions(directory):
+def read_questions(directory):
     path = Path(directory)
-    questionsDict = {}
+    questions_dict = {}
     for file in sorted(list(path.glob("*.*"))):
-        with open(file) as file:
-            questions = file.read().strip().split("\n#")
+        with open(file) as input_file:
+            questions = input_file.read().strip().split("\n#")
             questions[1:] = ["#" + question for question in questions[1:]]
-            questionsDict[file.name] = questions
+            questions_dict[file.name] = questions
 
-    return questionsDict
+    return questions_dict
 
 
-def generateQuestions(students, questions, *, num_each=2):
+def generate_questions(students, questions, *, num_each=2):
     files = dict()
     for group, lst in students.items():
-        for transliterate, original in lst:
-            files[f"{group}_{transliterate}.rst"] = f"{SEP}{original}\n{SEP}\n"
+        for trnslt, origin in lst:
+            files[f"{group}_{trnslt}.rst"] = f"{SEP}{origin}\n{SEP}\n"
 
-    for categoryName, category in questions.items():
-        print(f"{categoryName:<40}", end="\t")
-        personal = list(itertools.combinations(category, numEach))
+    for category_name, category in questions.items():
+        print(f"{category_name:<40}", end="\t")
+        personal = list(itertools.combinations(category, num_each))
         print(f"{len(personal)} combinations")
         assert len(personal) >= len(files)
         random.shuffle(personal)
-        for f in files:
-            files[f] += "\n" + "\n".join(personal.pop(0)) + "\n"
+        for file in files:
+            files[file] += "\n" + "\n".join(personal.pop(0)) + "\n"
 
     return files
 
 
-def write_files(outputDir, files):
-    outDir = Path(outputDir)
-    outDir.mkdir(existOk=True)
+def write_files(output_dir, files):
+    out_dir = Path(output_dir)
+    out_dir.mkdir(exist_ok=True)
     for filename, contents in files.items():
-        file = outDir.joinpath(filename)
+        file = out_dir.joinpath(filename)
         with open(file, "x") as out:
             out.write(contents)
 
@@ -76,4 +76,4 @@ if __name__ == "__main__":
     students_dict = parse_students(student_list)
     questions_dict = read_questions(questions_dir)
     cont = generate_questions(students_dict, questions_dict, num_each=2)
-    write_files(output, cont)
+    write_files(out_dir, cont)
