99921207edd3102f69206c2af7946ea1915ed66e
Lifad <shyrokov.vlad@gmail.com>
2022-01-19 15:57:52 +0200

    feat: mkr2 Shyrokov

--- dk91_shyrokov/lab_2/lab2_oop.py
+++ dk91_shyrokov/lab_2/lab2_oop.py
@@ -1,29 +1,29 @@
-class Musical_Group():
-    def __init__ (self, group_name, singer, song):
+class MusicalGroup():
+    def __init__(self,  singer, song, group_name=None):
         self.group_name = group_name
         self.singer = singer
         self.song = song
 
-    def what_the_song (self):
+    def what_the_song(self):
         print(f"\n{self.singer} from the group '{self.group_name}', wrote a song called '{self.song}'.")
 
 
-class Song(Musical_Group):
+class Song(MusicalGroup):
     def __init__(self, group_name, singer, song, topic, main_character):
-            Musical_Group. __init__(self, group_name, singer, song)
+            super(). __init__(self, group_name, singer, song)
             self.topic = topic
             self.main_character = main_character
 
-    def name_of_song (self):
+    def name_of_song(self):
              print(f"{self.group_name} {self. singer} {self.song} {self.topic} {self.main_character}")
 
-    def character (self):
+    def character(self):
                print(f"\nThe main character of this song, is a {self.main_character}.")
 
 
 class Sense(Song):
-     def __init__ (self, group_name, singer, song, topic, main_character, sense="passion and its consequences."):
-            Song. __init__(self, group_name, singer, song, topic, main_character )
+     def __init__(self, group_name, singer, song, topic, main_character, sense="passion and its consequences."):
+            super(). __init__(self, group_name, singer, song, topic, main_character)
             self.sense = sense
 
      def print_sense(self):
@@ -31,7 +31,7 @@ class Sense(Song):
 
 
 if __name__ == '__main__':  
-        group = Musical_Group("King and the Clown", "Andrew Knyazev (Knyaz)", "Joker")
+        group = MusicalGroup("King and the Clown", "Andrew Knyazev (Knyaz)", "Joker")
         group.what_the_song()
 
         song = Song("King and the Clown", "Andrew Knyazev (Knyaz)", "Joker", "passion for gambling", "gambler")
